3d3d306dcbb6af81dbe6a04c8a1d84233255f75e
Kuso0taku <denustuzan@gmail.com>
2025-10-04 21:20:34 +0700

    more changes

--- sem1/pract1/execute.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-task="task$1"
-path="./$task"
-echo "Making & executing $task:"
-echo
-nvim "$task.c"
-gcc "$task.c" -o "$task" && "$task"
--- sem1/pract1/execute_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-path="./task"
-for i in {1..8}; do 
-  echo "      task no.$i:"
-  "$path$i"
-  echo
-done
