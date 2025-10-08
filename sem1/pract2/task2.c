74a6b9b478c8a45a579f98d098bfe7ec42e95563
Kuso0taku <denustuzan@gmail.com>
2025-10-08 20:32:21 +0700

    task2 changed

--- sem1/pract2/task2.c
+++ sem1/pract2/task2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int i;
+  int ch;
   printf("Enter symbol in ASCII (int num): ");
-  scanf("%d", &i);
-  char ch = i;
+  scanf("%d", &ch);
   printf("Your symbol is %c\n", ch);
   return 0;
 }
