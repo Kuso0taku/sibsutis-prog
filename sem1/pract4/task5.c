43a7881080ea8eabf7af7e80665dc196e09b868c
Kuso0taku <denustuzan@gmail.com>
2025-10-18 00:32:55 +0700

    pract4 task5

---
+++ sem1/pract4/task5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int count=0;
+  int sum=0;
+  int n;
+  printf("Enter number of times to sum: ");
+  scanf("%d", &n);
+  while (count++ < n) {sum += count;}
+  printf("sum = %d\n", sum);
+
+  return 0;
+}
