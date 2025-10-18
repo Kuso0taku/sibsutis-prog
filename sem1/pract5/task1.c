54eafd05998bd113dcd3deaf0b258c0d114e5c44
Kuso0taku <denustuzan@gmail.com>
2025-10-19 00:23:19 +0700

    a little change task1

--- sem1/pract5/task1.c
+++ sem1/pract5/task1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  // (char)97 is 'a' 
-  char ch = 97;
+  char ch = 'a';
   char arr[26];
   for (int i=0;i<26;i++) {arr[i]=ch++;}
   for (int i=0;i<26;i++) {printf("%c", arr[i]);}
