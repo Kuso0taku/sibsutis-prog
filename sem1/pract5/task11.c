6a3ffcc8a9f206342b8a006a56c5e882881a47bc
Kuso0taku <denustuzan@gmail.com>
2025-10-19 12:12:52 +0700

    task11 pract5

---
+++ sem1/pract5/task11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int arr[8];
+  printf("Enter 8 int numbers: ");
+  for (int i=0; i<8; i++) {scanf("%d", &arr[i]);}
+  printf("Your nubmer: ");
+  for (int i=7; i>=0; i--) {printf("%d ", arr[i]);}
+  
+  return 0;
+}
