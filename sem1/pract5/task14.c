f1b689d60a8be3ff6c1d3d38842981dea58efd1b
Kuso0taku <denustuzan@gmail.com>
2025-10-19 13:06:42 +0700

    task14 pract5

---
+++ sem1/pract5/task14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  double arr1[8];
+  double arr2[8];
+  double sum=0;
+  printf("Enter 8 double elements of array: ");
+  for (int i=0; i<8; i++) {
+    scanf("%lf", &arr1[i]);
+    sum += arr1[i];
+    arr2[i]=sum;
+  }
+  printf("Result of program:\n");
+  for (int i=0; i<8; i++) {printf("%-20lf ", arr1[i]);}
+  printf("\n");
+  for (int i=0; i<8; i++) {printf("%-20lf ", arr2[i]);}
+  printf("\n");
+  
+  return 0;
+}
