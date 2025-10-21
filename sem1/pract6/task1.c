8aebc3a80d02b9cfbe39adddaee0b61d681dd947
Kuso0taku <denustuzan@gmail.com>
2025-10-21 23:38:40 +0700

    task1 pract6

---
+++ sem1/pract6/task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define SPACE ' '
+#define NEWLINE '\n'
+
+int main() {
+  unsigned short n_spc=0, n_nline=0, n_other=0;
+  char ch;
+  while ((ch = getchar()) != '#') {
+    switch (ch) {
+      case SPACE:
+        n_spc++;
+        break;
+      case NEWLINE:
+        n_nline++;
+        break;
+      default:
+        n_other++;
+    }
+  }
+  printf("Number of\n\tspaces: %hu\n\tnewlines: %hu\n\tother: %hu\n",
+         n_spc, n_nline, n_other);
+  
+  return 0;
+}
