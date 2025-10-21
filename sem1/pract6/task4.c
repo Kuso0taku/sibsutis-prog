1f6e617006171f133c5285b474daab55c2dd895e
Kuso0taku <denustuzan@gmail.com>
2025-10-22 00:24:51 +0700

    task4 pract6

---
+++ sem1/pract6/task4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  unsigned short count=0;
+  char ch;
+  printf("Input smth here ('#' to exit):\n");
+  while ((ch=getchar()) != '#') {
+    if (ch=='.') {printf("!");count++;}
+    else if (ch=='!') {printf("!!");count++;}
+    else {printf("%c", ch);}
+  }
+  printf("\nReplaced %hu characters\n", count);
+  
+  return 0;
+}
