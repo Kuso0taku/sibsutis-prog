d805c38bb4d84bf843157cbabcd7798f16ff922c
Kuso0taku <denustuzan@gmail.com>
2025-10-31 03:07:20 +0700

    task3 pract7

---
+++ sem1/pract7/task3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void chlines(char ch, int i, int j) {
+  for (int l=0; l<j; l++) {
+    for (int k=0;k<i;k++) putchar(ch);
+    putchar('\n');
+  }
+}
+
+int main() {
+  int code;
+  char ch;
+  int i,j;
+  do {
+    printf("Enter a char here: ");
+    code = scanf(" %c", &ch);
+    printf("Enter a char and 2 int numbers here: ");
+    if (code==1) {code += scanf("%d%d", &i, &j);}
+    else {break;}
+    if (code==3) {
+      printf("The result of program:\n");
+      chlines(ch, i, j);
+      printf("\n");
+    }
+  } while (code == 3);
+  printf("\nOops! Wrong input!\n");
+
+
+  return 0;
+}
