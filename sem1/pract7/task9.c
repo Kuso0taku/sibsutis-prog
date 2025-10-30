40132300d40753e8d27113ed3d59f94564f2c4f2
Kuso0taku <denustuzan@gmail.com>
2025-10-31 04:10:21 +0700

    task9 pract7

---
+++ sem1/pract7/task9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+double power(double a, int n) {
+  if (a==0 && n==0) {printf("Error: 0^0 is not defined!\n"); return 1;}
+  if (a==0) return 0;
+  if (n==0) return 1;
+  if (n>0) return a*power(a, n-1);
+  else return 1/a*power(a, n+1);
+}
+
+int main() {
+  double a;
+  int n, code0, code1;
+  printf("The program pows a double number.\n");
+  for (int i=0; i<25; i++) putchar('-'); 
+  putchar('\n');
+  do {
+    printf("Enter a number to pow: ");
+    code0 = scanf("%lf", &a);
+    printf("Enter a power: ");
+    if (code0==1 && (code1=scanf("%d", &n))==1)
+    printf("(%.2lf)^(%d)=%.2lf\n", a, n, power(a,n));
+    for (int i=0; i<25; i++) putchar('-'); 
+    putchar('\n');
+  } while (code0==1 && code1==1);
+  printf("Oops! Wrong input!\n");
+  return 0;
+}
