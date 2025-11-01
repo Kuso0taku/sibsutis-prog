4c6f4a5f9d0c6b7743394c9d8ab957a2ba8e64a5
Kuso0taku <denustuzan@gmail.com>
2025-11-01 19:52:15 +0700

    task5 pract8

---
+++ sem1/pract8/task5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double dif_max_min(double arr[], int n) {
+  double mi=*arr, ma=*arr;
+  for (int i=0; i<n; i++, arr++) {
+    ma = (ma>=*arr) ? ma : *arr;
+    mi = (mi<=*arr) ? mi : *arr;
+  }
+  return ma-mi;
+}
+
+int main() {
+  int size;
+  printf("Enter the size of the array: ");
+  while (!(scanf("%d", &size)) || size <= 0) 
+    printf("Wrong input! Enter positive int: ");
+
+  printf("Enter every element of the array: ");
+  double arr[size];
+  double *ptr = &arr[0];
+  for (int i=0; i<size; i++, ptr++) scanf("%lf", ptr);
+  
+  printf("The difference of max and min elements of your array: %lf\n", 
+         dif_max_min(arr, size));
+
+  return 0;
+}
