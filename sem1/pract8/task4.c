a5170583fc1eea2f140fd45c14aca93aca54118b
Kuso0taku <denustuzan@gmail.com>
2025-11-01 19:46:41 +0700

    task4 pract8

---
+++ sem1/pract8/task4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int max_i(double arr[], int n) {
+  int m_i=0;
+  for (int i=0; i<n; i++) m_i = (arr[m_i] >= arr[i]) ? m_i : i;
+  return m_i;
+}
+
+int main() {
+  int size;
+  printf("Enter the size of the array: ");
+  while (!(scanf("%d", &size)) || size <= 0) 
+    printf("Wrong input! Enter positive int: ");
+
+  printf("Enter every double element of the array: ");
+  double arr[size];
+  double *ptr = &arr[0];
+  for (int i=0; i<size; i++, ptr++) scanf("%lf", ptr);
+  
+  printf("The max element index of your array: %d\n", max_i(arr, size));
+
+  return 0;
+}
