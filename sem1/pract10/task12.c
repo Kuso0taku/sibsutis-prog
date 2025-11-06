e02cf16b8eee84b2f91f326f681e442153bc62fd
Kuso0taku <denustuzan@gmail.com>
2025-11-07 00:53:43 +0700

    task12 pract10

---
+++ sem1/pract10/task12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+  int cnt_w=0, cnt_up=0, cnt_low=0, cnt_pnc=0, cnt_dgt=0;
+  char ch;
+  while ((ch=getchar()) != EOF) {
+    if (isdigit(ch)) cnt_dgt++;
+    else if (islower(ch)) cnt_low++;
+    else if (isupper(ch)) cnt_up++;
+    else if (ispunct(ch)) cnt_pnc++;
+    else if (isspace(ch)) cnt_w++;
+  }
+  printf("Number of:\n\twords: %d\n\tuppercase: %d\n\tlowercase: %d\n\t\
+punctuations: %d\n\tdigits: %d\n", cnt_w, cnt_up, cnt_low, cnt_pnc, cnt_dgt);
+}
