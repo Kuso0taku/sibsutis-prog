9a975db7f06a45effc57c14110eec849f5ca9b21
Kuso0taku <denustuzan@gmail.com>
2025-11-14 20:55:09 +0700

    lil chng .h file again

--- sem1/pract8/myarrfunc.h
+++ sem1/pract8/myarrfunc.h
@@ -1,4 +1,4 @@
 int fnd_lst_pstv(int *arrptr, size_t n); // finds last positive element
 int fnd_max(int *arrptr, size_t n); // finds max element
-size_t cnt_mltpl_k(int *arrptr, size_t n, int k); // counts elements multiple k
+size_t cnt_mltpl_k(int *arrptr, size_t n, int k); // counts elements multiple of k
 size_t cnt_avg_grt(int *arrptr, size_t n); // counts elements greater than average value of the array
