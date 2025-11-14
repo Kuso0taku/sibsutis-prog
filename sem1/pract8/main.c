3204b1a6e886df6e6d28947384a9f200dd4e43db
Kuso0taku <denustuzan@gmail.com>
2025-11-14 22:25:22 +0700

    add main.c

---
+++ sem1/pract8/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <ctype.h> // tolower()
+#include "myarrfunc.h"
+
+int main() {
+  // init size of array
+  int n=0;
+  printf("Enter array length: ");
+  while(scanf("%d", &n) != 1 || n<=0) {
+    printf("Wrong input! The num must be >= 0! Enter correct value here: ");
+    while (getchar()!='\n');
+  }
+  
+  // init array
+  int arr[n];
+  for (int i=0; i<n; i++) {
+    printf("Enter the %d element: ", i+1);
+    while(scanf("%d", arr+i) != 1) {
+      printf("Wrong input! Enter correct value here: ");
+      while (getchar()!='\n');
+    }
+  }
+  
+  putchar('\n');
+  int choice=0, k=0, ch=0;
+  int m=fnd_max(arr, n);
+  do {
+    // prints menu
+    for (int i=0; i<35; i++) putchar('*');
+    putchar('\n');
+    puts("(1) Find last positive element");
+    puts("(2) Count elements multiple of k");
+    puts("(3) Count elements greater than average value of the array");
+    puts("(4) Exit");
+    for (int i=0; i<35; i++) putchar('*');
+    putchar('\n');
+
+    // reads choice
+    printf("Enter your choice: ");
+    while(scanf("%d", &choice) != 1 || choice<1 || choice>4) {
+      printf("Wrong input! The input must be a number from 1 to 4!\n"
+             "Enter correct value here: ");
+      while (getchar()!='\n');
+    }
+    
+    // choice processing
+    if (choice==4) break;
+    switch (choice) {
+      case 1: {
+        printf("The last positive elements: %d\n", fnd_lst_pstv(arr, n));
+        break;
+      }
+      case 2: {
+        //reads k 
+        printf("Enter k here: ");
+        while(scanf("%d", &k) != 1 || k>m) {
+          printf("Wrong input! The k must be <= max value of the array (%d)!"
+                 "\nEnter correct value here: ", m);
+          while (getchar()!='\n');
+        }
+
+        printf("Number of elements multiple of %d: %d\n", 
+               k, cnt_mltpl_k(arr, n, k));
+        break;
+      }
+      case 3: {
+        printf("Number of elements greater than average value of the array: %d\n", 
+               cnt_avg_grt(arr, n));
+        break;
+      }
+    }
+    
+    // clears buffer from all inputs
+    while (getchar()!='\n');
+
+    // continue
+    printf("Do you want to continue? (Y/n): ");
+    ch=tolower(getchar());
+    if (ch=='n') choice=4;
+    if (ch!='\n') while (getchar()!='\n');
+    putchar('\n');
+  } while (choice != 4);
+  puts("Done.");
+
+  return 0;
+}
