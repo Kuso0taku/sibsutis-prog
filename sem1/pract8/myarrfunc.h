b29a98696f3127598f20f83ba83b7da44d213b7a
Kuso0taku <denustuzan@gmail.com>
2025-11-21 14:32:27 +0700

    .h chngd

--- sem1/pract8/myarrfunc.h
+++ sem1/pract8/myarrfunc.h
@@ -1,6 +1,8 @@
 #include <stddef.h>
 
+void print_arr_val(int *arrptr, size_t n); // prints values of array 
+void print_arr_ptr(int *arrptr, size_t n); // prints addresses of array
 int fnd_lst_pstv(int *arrptr, size_t n); // finds last positive element
 int fnd_max(int *arrptr, size_t n); // finds max element
-size_t cnt_mltpl_k(int *arrptr, size_t n, int k); // 
+size_t cnt_mltpl_k(int *arrptr, size_t n, int k); // counts elements multiple of k
 size_t cnt_avg_grt(int *arrptr, size_t n); // counts elements greater than average value of the array
