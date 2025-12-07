ec721fc882e11658c9d7bef7e8bb0dac31cf87cc
Kuso0taku <denustuzan@gmail.com>
2025-12-07 19:56:12 +0700

    .h cmt chng

--- sem1/pract10/stringfunc.h
+++ sem1/pract10/stringfunc.h
@@ -1,6 +1,6 @@
 #include <wchar.h>
 
-int cnt_chs(wchar_t *text); // counts how many each other characters
+int cnt_chs(wchar_t *text); // counts how many every character
 void ins_exclm_mark(wchar_t *text); // inserts exclemation mark in the on of each line
 int cnt_dash_wrds(wchar_t *text); // finds and counts words with dashes
 void ins_c_after_vwl(wchar_t *text); // inserts 'с' after vowel and repeats vowel
