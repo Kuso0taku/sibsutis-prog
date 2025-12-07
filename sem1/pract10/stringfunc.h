bcac7fdd92fc76d3b24931422be2a62082f66c24
Kuso0taku <denustuzan@gmail.com>
2025-12-07 17:45:48 +0700

    key chng .h again

--- sem1/pract10/stringfunc.h
+++ sem1/pract10/stringfunc.h
@@ -1,4 +1,6 @@
-int cnt_chs(char *text); // counts how many each other characters
-void ins_exclm_mark(char *text); // inserts exclemation mark in the on of each line
-int cnt_dash_wrds(char *text); // finds and counts words with dashes
-void ins_c_after_vwl(char *text); // inserts 'с' after vowel and repeats vowel
+#include <wchar.h>
+
+int cnt_chs(wchar_t *text); // counts how many each other characters
+void ins_exclm_mark(wchar_t *text); // inserts exclemation mark in the on of each line
+int cnt_dash_wrds(wchar_t *text); // finds and counts words with dashes
+void ins_c_after_vwl(wchar_t *text); // inserts 'с' after vowel and repeats vowel
