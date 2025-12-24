19e7f7c42217b4568b00c4295286f056b1babc52
Kuso0taku <denustuzan@gmail.com>
2025-12-24 23:11:29 +0700

    exam task2

--- sem1/exam/main.c
+++ sem1/exam/main.c
@@ -3,7 +3,9 @@
 # include <locale.h>
 # include <wctype.h> // for iswspace
 # include <math.h> // pow
+#define N 10 // 1D array size
 
+// task1
 int ccw2_10(const wchar_t *wstr, size_t len) {
   int num=0;
   for (size_t i=len-1; i>=0 && 
@@ -13,10 +15,20 @@ int ccw2_10(const wchar_t *wstr, size_t len) {
   return num;
 }
 
+// task2
+int sum_odd(int *A, size_t n) {
+  int sum=0, num=0;
+  for (size_t i=0; i<n; i++) {
+    num = *(A+i);
+    if (num%2) sum+=num;
+  }
+  return sum;
+}
+
 int main () {
   setlocale(LC_ALL, "");
 
-  // input task1
+  // task1
   wchar_t buffer[255] = {0};
   wchar_t ch=0;
   size_t len=0;
@@ -25,6 +37,14 @@ int main () {
     (ch==L'0' || ch==L'1'); len++) 
     *(buffer+len)=ch;
   wprintf(L"out: %d\n", ccw2_10(buffer, len));
-  
+
+  // task2 
+  int code=0;
+  int A[N] = {0};
+  fputws(L"\nTask #2\nin: ", stdout);
+  for (size_t i=0; i<N; i++)
+    while ((code=wscanf(L"%d", (A+i)))!=1) while (!iswspace(ch=getwchar()));
+  wprintf(L"out: %d\n", sum_odd(A, N));
+
   return 0;
 }
