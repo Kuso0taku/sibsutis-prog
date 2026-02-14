f25eb30711e93f66334824efb0419cdd500928c3
Kuso0taku <denustuzan@gmail.com>
2026-02-14 21:10:24 +0700

    01 .h lil chng

--- sem2/01-struct/struct.h
+++ sem2/01-struct/struct.h
@@ -19,11 +19,11 @@ int matrix2d_scanf(Matrix2D*);
 void matrix2d_wprintf(Matrix2D*);
 void matrix2d_increment(Matrix2D*);
 void matrix2d_decrement(Matrix2D*);
-void matrix2d_setter(Matrix2D*);
+void matrix2d_setter(Matrix2D*, const size_t, const size_t);
 void matrix2d_random(Matrix2D*);
 // grade "Good"
-float* matrix2d_get_row(Matrix2D*);
-float* matrix2d_get_col(Matrix2D*);
+float* matrix2d_get_row(Matrix2D*, const size_t);
+float* matrix2d_get_col(Matrix2D*, const size_t);
 float matrix2d_determinant(Matrix2D*);
 // grade "Excellent"
 Matrix2D* matrix2d_inverse(Matrix2D*);
