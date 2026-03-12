45294ac1d1834904a4a46de1b87f224cd8c4349f
Kuso0taku <denustuzan@gmail.com>
2026-03-12 21:45:35 +0700

    02 bitstruct.h

---
+++ sem2/02-bits/bitstruct.h
@@ -0,0 +1,9 @@
+typedef struct {
+    unsigned int red : 8;
+    unsigned int gren : 8;
+    unsigned int blue : 8;
+    unsigned int bright : 8;
+    unsigned int temp : 14;
+    unsigned int type : 2;
+    unsigned int mode : 2;
+} rgbled;
